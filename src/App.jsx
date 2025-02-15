5ed119d1003b59c61fe0f5b32b88fe5a4c3e9369
msuzun <sevki_uzun53@hotmail.com>
2025-02-15 11:35:05 +0300

    Tailwind installation and setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Button } from 'antd'
 function App() {
 
   return (
-   <div>
+   <div className='flex justify-center h-screen items-center flex-col bg-red-500'>
       <h1>
         SheyTracker
       </h1>
