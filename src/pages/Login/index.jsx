9a4211d8115a47433e0cf2a748dc2a9fe3b58ada
msuzun <sevki_uzun53@hotmail.com>
2025-02-16 11:39:01 +0300

    Registration Screen

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -3,24 +3,31 @@ import { Form, Input, Button } from "antd"
 import { Link } from 'react-router-dom'
 import Divider from '../../components/Divider'
 const Login = () => {
-  const onFinish = (values)=>{
-    console.log("Success: ",values);
+  const onFinish = (values) => {
+    console.log("Success: ", values);
   }
   return (
     <div className='grid grid-cols-2'>
-      <div className='bg-primary h-screen'>
-
+      <div className='bg-primary h-screen flex flex-col justify-center items-center'>
+        <div>
+          <h1 className="text-7xl text-white">
+            SHEY-TRACKER
+          </h1>
+          <span className='text-white mt-5'>
+            One place to track all your business records
+          </span>
+        </div>
       </div>
       <div className='flex justify-center items-center'>
         <div className='w-[420px]'>
           <h1 className='text-2xl text-gray-700'>LOGIN TO YOUR ACCOUNT</h1>
-          <Divider/>
+          <Divider />
           <Form layout='vertical' onFinish={onFinish}>
             <Form.Item label="Email" name="email">
               <Input />
             </Form.Item>
             <Form.Item label="Password" name="password">
-              <Input type='password'/>
+              <Input type='password' />
             </Form.Item>
             <Button type='primary' htmlType='submit' block>Login</Button>
             <div className="flex justify-center mt-5">
