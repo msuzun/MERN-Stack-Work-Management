899e8f64fa2bb19ee1757ded437c21d9b876911e
msuzun <sevki_uzun53@hotmail.com>
2025-02-16 18:11:19 +0300

    Create Profile Pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Register from "./pages/Register"
 import Login from "./pages/Login"
 import Home from "./pages/Home"
 import ProtectedPage from "./components/ProtectedPage"
+import Profile from "./pages/Profile"
 function App() {
 
   return (
@@ -10,6 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<ProtectedPage><Home /></ProtectedPage>} />
+          <Route path="/profile" element={<ProtectedPage><Profile /></ProtectedPage>} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
--- src/components/ProtectedPage.jsx
+++ src/components/ProtectedPage.jsx
@@ -34,13 +34,33 @@ function ProtectedPage({ children }) {
     }, [])
     return (
         <App>
-            {contextHolder}
-            <div>
-                <h1>ProtectedPage</h1>
-                <h1>Welcome {user?.firstName} {user?.lastName}</h1>
-                {children}
-            </div>
-        </App>
+        {contextHolder}
+        <div className="min-h-screen flex flex-col">
+            <header className="flex justify-between items-center bg-primary text-white m-px-5 m-py-4">
+                <h1 className="text-2xl font-bold ml-5 cursor-pointer" onClick={()=>{
+                    navigate("/")
+                }}>SHEY-TRACKER</h1>
+                <div className="flex items-center bg-white text-primary rounded shadow mr-10 mb-5 mt-5">
+                    <span className="mr-2 ml-2 mt-2 mb-2 cursor-pointer underline" onClick={()=>{
+                        navigate("/profile")
+                    }}>{user?.firstName}</span>
+                    <i className="ri-notification-line text-xl bg-gray-300  p-2 rounded-full cursor-pointer" onClick={()=>{
+                        
+                    }}></i>
+                    <i className="ri-logout-box-line text-xl ml-10 cursor-pointer" onClick={()=>{
+                        localStorage.removeItem("token");
+                        navigate("/login");
+                    }}></i>
+                </div>
+            </header>
+            <main className="flex-grow p-5">
+                <div className='ml-5 mt-2'>
+                    {children}
+                </div>
+                
+            </main>
+        </div>
+    </App>
 
     )
 }
